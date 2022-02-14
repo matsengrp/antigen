6feaa41d9be0e54ccc38a72c82e3ab6f226d9755
zorian15 <zoriant15@gmail.com>
2022-02-14 08:20:25 -0800

    stems for unit tests

---
+++ TestSequencePhenotype.java
@@ -0,0 +1,64 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author zorian15
+ * @date 2.14.2022
+ * 
+ * A class for testing the methods in SequencePhenotype.java
+ */
+public class TestSequencePhenotype {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	@Test
+	public void testConstructors() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDistance() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSetAndGetSequence() {
+		fail("Not yet implemented");
+		
+	}
+	
+	@Test
+	public void testMutate() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testRiskOfInfection() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testToSTring() {
+		fail("Not yet implemented");
+	}
+
+}
