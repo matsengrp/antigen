ba35880ff8f138cb1550c04580e4dd5dfff9a220
thienktran <thientran154221@gmail.com>
2022-02-22 08:36:03 -0800

    Only include relevant changes to the current branch

--- PhenotypeFactory.java
+++ PhenotypeFactory.java
@@ -6,48 +6,43 @@ public class PhenotypeFactory {
 	public static String GEOMETRIC = "geometric";
 	public static String GEOMETRIC3D = "geometric3d";
 	public static String GEOMETRIC10D = "geometric10d";
-	public static String SEQUENCE = "sequence";
 
 	// returns newly instantiated Phenotype objects of type according to Parameters.phenotypeSpace
 	public static Phenotype makeVirusPhenotype() {
-	
-		Phenotype p = null;	
-		if (GEOMETRIC.equals(Parameters.phenotypeSpace)) { p = new GeometricPhenotype(); }	
-		if (GEOMETRIC3D.equals(Parameters.phenotypeSpace)) { p = new GeometricPhenotype3D(); }
-		if (GEOMETRIC10D.equals(Parameters.phenotypeSpace)) { p = new GeometricPhenotype10D(); }
-		if (SEQUENCE.equals(Parameters.phenotypeSpace)) { p = new SequencePhenotype(); }
+
+		Phenotype p = null;
+		if (GEOMETRIC.equals(Parameters.phenotypeSpace)) { p = new GeometricPhenotype(); }
+		if ("geometric3d".equals(Parameters.phenotypeSpace)) { p = new GeometricPhenotype3D(); }
+		if ("geometric10d".equals(Parameters.phenotypeSpace)) { p = new GeometricPhenotype10D(); }
 		return p;
-	
+
 	}
-	
+
 	// returns newly instantiated Phenotype objects of type according to Parameters.phenotypeSpace
 	public static Phenotype makeHostPhenotype() {
-	
+
 		Phenotype p = null;
-		if (GEOMETRIC.equals(Parameters.phenotypeSpace)) {
-			p = new GeometricPhenotype(Parameters.initialTraitA, 0); 
-		}	
-		if (GEOMETRIC3D.equals(Parameters.phenotypeSpace)) {
-			p = new GeometricPhenotype3D(Parameters.initialTraitA, 0, 0); 
+		if ("geometric".equals(Parameters.phenotypeSpace)) {
+			p = new GeometricPhenotype(Parameters.initialTraitA, 0);
 		}
-		if (GEOMETRIC10D.equals(Parameters.phenotypeSpace)) {
+		if ("geometric3d".equals(Parameters.phenotypeSpace)) {
+			p = new GeometricPhenotype3D(Parameters.initialTraitA, 0, 0);
+		}
+		if ("geometric10d".equals(Parameters.phenotypeSpace)) {
 			double[] traits = {Parameters.initialTraitA, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-			p = new GeometricPhenotype10D(traits); 
-		}
-		if (SEQUENCE.equals(Parameters.phenotypeSpace)) {
-			p = new SequencePhenotype(Parameters.sequence);
+			p = new GeometricPhenotype10D(traits);
 		}
 		return p;
-	
-	}	
-	
+
+	}
+
 	// returns newly instantiated Phenotype objects of type according to Parameters.phenotypeSpace
 	public static Phenotype makeArbitaryPhenotype(double x, double y) {
-	
+
 		Phenotype p = null;
-		if (GEOMETRIC.equals(Parameters.phenotypeSpace)) { p = new GeometricPhenotype(x, y); }
+		if ("geometric".equals(Parameters.phenotypeSpace)) { p = new GeometricPhenotype(x, y); }
 		return p;
-	
-	}		
+
+	}
 
 }
