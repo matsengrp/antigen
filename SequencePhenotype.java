ec1eab90586be47dd9380b77b580dcfbad1dd160
thienktran <thientran154221@gmail.com>
2022-02-24 10:49:28 -0800

    Reduce Random object creation and hage return of mutate()

--- SequencePhenotype.java
+++ SequencePhenotype.java
@@ -3,7 +3,6 @@ import java.lang.*;
 public class SequencePhenotype implements Phenotype {
 
     // constants
-    @SuppressWarnings("unchecked")
     public final String[] NUCLEOBASES = new String[]{"A", "C", "G", "T"};
 
     // fields
@@ -12,9 +11,8 @@ public class SequencePhenotype implements Phenotype {
     // constructor
     public SequencePhenotype() {
         this.sequence = "";
-        Random rand = new Random();
         for (int i = 0; i < Parameters.sequence.length(); i++) {
-            int indexN = rand.nextInt(0, this.NUCLEOBASES.length - 1);
+            int indexN = random(this.NUCLEOBASES.length);
             this.sequence += this.NUCLEOBASES[indexN];
         }
     }
@@ -22,7 +20,7 @@ public class SequencePhenotype implements Phenotype {
         sequence = sequence.toUpperCase();
         for (int i = 0; i < sequence.length(); i++) {
             String sequenceChar = ("" + sequence.charAt(i));
-            boolean contains = Arrays.stream(NUCLEOBASES).anyMatch(sequenceChar::equals);
+            boolean contains = java.util.Arrays.asList(NUCLEOBASES).contains(sequenceChar);
             if (!contains) {
                 throw new IllegalArgumentException(sequenceChar + " is not a valid nucleobase!");
             }
@@ -50,7 +48,7 @@ public class SequencePhenotype implements Phenotype {
         // equal length validation
         if (this.sequence.length() != seq2.length()) {
             System.out.println("s1 " + this.sequence);
-            System.out.println("s2 " + seq2.toString());
+            System.out.println("s2 " + seq2);
             throw new IllegalArgumentException("Sequence lengths are not equal!");
         }
 
@@ -62,7 +60,7 @@ public class SequencePhenotype implements Phenotype {
             }
         }
 
-        return (double) hammingDistance;
+        return hammingDistance;
     }
 
     // cross immunity between a virus phenotype and a host's immune history
@@ -79,10 +77,9 @@ public class SequencePhenotype implements Phenotype {
     }
 
     // returns a mutated copy, original SequencePhenotype is unharmed
-    public Phenotype mutate() {
-        Random rand = new Random();
-        int index = rand.nextInt(0, this.sequence.length() - 1);
-        int indexN = rand.nextInt(0, this.NUCLEOBASES.length - 1);
+    public SequencePhenotype mutate() {
+        int index = random(this.sequence.length());
+        int indexN = random(this.NUCLEOBASES.length);
 
         // substitute a random index of sequence with a random nucleobase
         StringBuilder mutated = new StringBuilder(this.sequence);
@@ -95,8 +92,8 @@ public class SequencePhenotype implements Phenotype {
         return this.sequence;
     }
 
-    private int random() {
+    private int random(int length) {
         Random random = new Random();
-        return random.nextInt(0, NUCLEOBASES.length - 1);
+        return random.nextInt(0, length - 1);
     }
 }
