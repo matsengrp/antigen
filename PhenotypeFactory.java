f9f0b12b96c0a8880c86a9e05fa3fe031dfda7d2
thienktran <92776467+thienktran@users.noreply.github.com>
2022-03-07 15:07:31 -0800

    Cluster out.tips using sklearn (#9)
    * Resolving issue #1. Enable mutate() to substitute
    
    * Update Host to have SequencePhenotype
    
    * Updated parameters and PhenotypeFactory to work with "sequence"
    
    * Wrote Python script to cluster using sklearn instead of Mathematica
    
    * Only include relevant changes to the current branch
    
    * Only include relevant files
    
    * Remove changes from parameters.yml
    
    * Add png
    
    * Remove csv functionality from Simulation.java - clustering with out.tips not out.timeseries
    
    * Update in order to reflect main
    
    * Update Host to have SequencePhenotype
    
    * Updated parameters and PhenotypeFactory to work with "sequence"
    
    * Wrote Python script to cluster using sklearn instead of Mathematica
    
    * Only include relevant changes to the current branch
    
    * Only include relevant files
    
    * Remove changes from parameters.yml
    
    * Add png
    
    * Remove csv functionality from Simulation.java - clustering with out.tips not out.timeseries
    
    * Update in order to reflect main
    
    * Address PR Feedback - argparse
    
    * Add CSV logic in Simulation and example files back
    
    * Change out_timeseries.csv file path
    
    Co-authored-by: Zorian Thornton <zoriant15@gmail.com>

--- PhenotypeFactory.java
+++ PhenotypeFactory.java
@@ -5,15 +5,15 @@ public class PhenotypeFactory {
 
 	public static String GEOMETRIC = "geometric";
 	public static String GEOMETRIC3D = "geometric3d";
-	public static String GEOMETRIC10D = "geometric10d";	
+	public static String GEOMETRIC10D = "geometric10d";
 
 	// returns newly instantiated Phenotype objects of type according to Parameters.phenotypeSpace
 	public static Phenotype makeVirusPhenotype() {
 	
 		Phenotype p = null;	
 		if (GEOMETRIC.equals(Parameters.phenotypeSpace)) { p = new GeometricPhenotype(); }	
-		if ("geometric3d".equals(Parameters.phenotypeSpace)) { p = new GeometricPhenotype3D(); }		
-		if ("geometric10d".equals(Parameters.phenotypeSpace)) { p = new GeometricPhenotype10D(); }				
+		if ("geometric3d".equals(Parameters.phenotypeSpace)) { p = new GeometricPhenotype3D(); }
+		if ("geometric10d".equals(Parameters.phenotypeSpace)) { p = new GeometricPhenotype10D(); }
 		return p;
 	
 	}
@@ -22,16 +22,16 @@ public class PhenotypeFactory {
 	public static Phenotype makeHostPhenotype() {
 	
 		Phenotype p = null;
-		if ("geometric".equals(Parameters.phenotypeSpace)) { 
+		if ("geometric".equals(Parameters.phenotypeSpace)) {
 			p = new GeometricPhenotype(Parameters.initialTraitA, 0); 
 		}	
-		if ("geometric3d".equals(Parameters.phenotypeSpace)) { 
+		if ("geometric3d".equals(Parameters.phenotypeSpace)) {
 			p = new GeometricPhenotype3D(Parameters.initialTraitA, 0, 0); 
 		}
-		if ("geometric10d".equals(Parameters.phenotypeSpace)) { 
+		if ("geometric10d".equals(Parameters.phenotypeSpace)) {
 			double[] traits = {Parameters.initialTraitA, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 			p = new GeometricPhenotype10D(traits); 
-		}		
+		}
 		return p;
 	
 	}	
@@ -40,7 +40,7 @@ public class PhenotypeFactory {
 	public static Phenotype makeArbitaryPhenotype(double x, double y) {
 	
 		Phenotype p = null;
-		if ("geometric".equals(Parameters.phenotypeSpace)) { p = new GeometricPhenotype(x, y); }	
+		if ("geometric".equals(Parameters.phenotypeSpace)) { p = new GeometricPhenotype(x, y); }
 		return p;
 	
 	}		
