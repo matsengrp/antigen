8bc8381df710d44cf296572d7f757bced024a555
thienktran <thientran154221@gmail.com>
2022-07-07 13:53:35 -0700

    finish unit tests for geometricseq

--- TestGeometricSeqPhenotype.java
+++ TestGeometricSeqPhenotype.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 /**
  * A class for testing the methods in GeometricSeqPhenotype.java
@@ -20,24 +21,33 @@ public class TestGeometricSeqPhenotype {
     // Private fields
     private GeometricSeqPhenotype emptyPheno; // Default constructor
     private GeometricSeqPhenotype simplePheno; // Give a specific sequence
+    private GeometricSeqPhenotype simplePheno2; // Give a specific sequence
+
+    GeometricSeqPhenotype [] history;
 
     /**
      * Define variables needed for multiple tests.
      */
     @Before
     public void setUp() {
+        // initialize static parameters
+        Parameters.load();
+        Parameters.initialize();
+
         Random r = new Random();
         long seed = 5;
         r.setSeed(seed);
         emptyPheno = new GeometricSeqPhenotype();
         simplePheno = new GeometricSeqPhenotype(0.0, 0.0, new char[]{'T', 'G', 'C', 'T', 'A', 'G'});
 
-        // Create a small history of GeometricSeq phenotypes
-        GeometricSeqPhenotype [] history = new GeometricSeqPhenotype[3];
+        simplePheno2 = new GeometricSeqPhenotype(20.0, 10.0, new char[]{'T', 'G', 'C', 'T', 'A', 'G'});
 
-        history[0] = new GeometricSeqPhenotype(0.0, 2.0, new char[]{'T', 'G', 'C', 'T', 'A', 'G'});
-        history[1] = new GeometricSeqPhenotype(0.0, 2.0, new char[]{'T', 'G', 'C', 'T', 'A', 'G'});
-        history[2] = new GeometricSeqPhenotype(0.0, 2.0, new char[]{'T', 'G', 'C', 'T', 'A', 'G'});
+        // Create a small history of GeometricSeq phenotypes
+        history = new GeometricSeqPhenotype[3];
+
+        history[0] = new GeometricSeqPhenotype(1.0, 2.0, new char[]{'T', 'G', 'C', 'T', 'A', 'G'});
+        history[1] = new GeometricSeqPhenotype(3.0, 2.0, new char[]{'T', 'G', 'C', 'T', 'A', 'G'});
+        history[2] = new GeometricSeqPhenotype(5.0, 8.0, new char[]{'T', 'G', 'C', 'T', 'A', 'G'});
     }
 
     /**
@@ -45,9 +55,10 @@ public class TestGeometricSeqPhenotype {
      */
     @Test
     public void testConstructors() {
-        // For now, assert that an emptyConstructor returns a single nucleotide
+        assertEquals(0, emptyPheno.getSequence().length() % 3);
         assertEquals(Parameters.startingSequence.length(), emptyPheno.getSequence().length());
-        assertEquals("ACGT", simplePheno.getSequence());
+
+        assertEquals("TGCTAG", simplePheno.getSequence());
     }
 
     /**
@@ -64,12 +75,17 @@ public class TestGeometricSeqPhenotype {
      */
     @Test
     public void testGetSequence() {
-        assertEquals("TGCTAG", simplePheno.toString());
+        assertEquals("TGCTAG", simplePheno.getSequence());
     }
 
     @Test
     public void testDistance() {
+        assertEquals(2.236068, simplePheno.distance(history[0]), 0.000001);
+        assertEquals(3.605551, simplePheno.distance(history[1]), 0.000001);
+        assertEquals(9.433981, simplePheno.distance(history[2]), 0.000001);
 
+        assertEquals(7.211103, history[2].distance(history[0]), 0.000001);
+        assertEquals(7.211103, history[0].distance(history[2]), 0.000001);
     }
 
     /**
@@ -77,7 +93,9 @@ public class TestGeometricSeqPhenotype {
      */
     @Test
     public void testMutate() {
-
+        assertEquals("TGCTAG", simplePheno.getSequence());
+        GeometricSeqPhenotype mutantPheno = (GeometricSeqPhenotype) simplePheno.mutate();
+        assertNotEquals("TGCTAG", mutantPheno.getSequence());
     }
 
     /**
@@ -85,7 +103,17 @@ public class TestGeometricSeqPhenotype {
      */
     @Test
     public void testRiskOfInfection() {
+        // closestDistance * Parameters.smithConversion = 2.236068 * 0.1 = 0.2236068
+        // minRisk = 1.0 - Parameters.homologousImmunity = 1.0 - 0.95 = 0.05
+        // risk = Math.max(minRisk, risk) = max(0.05, 0.2236068) = 0.2236068
+        // risk = Math.min(1.0, risk) = 0.2236068
+        assertEquals(0.2236068, simplePheno.riskOfInfection(history), 0.00001);
 
+        // closestDistance * Parameters.smithConversion = 15.132746 * 0.1 = 1.5132746
+        // minRisk = 1.0 - Parameters.homologousImmunity = 1.0 - 0.95 = 0.05
+        // risk = Math.max(minRisk, risk) = max(0.05, 1.5132746) = 1.5132746
+        // risk = Math.min(1.0, risk) = 1.0
+        assertEquals(1.0, simplePheno2.riskOfInfection(history), 0.0);
     }
 
     /**
@@ -94,7 +122,8 @@ public class TestGeometricSeqPhenotype {
      */
     @Test
     public void testToString() {
-        assertEquals("TGCTAG, 0.0, 0.0", simplePheno.toString());
+        assertEquals("TGCTAG, 0.0000, 0.0000, 0, 0", simplePheno.toString());
+        assertNotEquals("TGCTAG, 0.0000, 0.0000, 0, 0", simplePheno.mutate().toString());
     }
 
     @Test
