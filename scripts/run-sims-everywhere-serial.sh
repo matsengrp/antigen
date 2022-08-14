a6f5fd746916508973e36bf67639e064511f0e6b
zorian15 <zoriant15@gmail.com>
2022-08-14 10:01:24 -0700

    pipeline to run simulations in a serial fashion

--- scripts/run-sims-everywhere-serial.sh
+++ scripts/run-sims-everywhere-serial.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 set -eu
 
+########### Setup variables ###########
 jar=$(find $PWD -name "antigen.jar");
+PYTHON_DIRS_SCRIPT=$(find $PWD -name "setup_dirs.py");
+IN_FILE=$(find $PWD -name $1 | head -n 1);
+EDIT_FILE=$(find $PWD -name $2);
 
-# Move to desired ouput directory
-cd ../python/results
+########### Create directory hierarchy ###########
+mkdir -p simulations;
+cd simulations;
+
+echo "LOG: Running python script to create directory hierachy.";
+python $PYTHON_DIRS_SCRIPT $IN_FILE $EDIT_FILE;
+echo "LOG: Simulation directory hierarchy creation done.";
+
+########### Run simulations ###########
+echo "LOG: Starting simulation...";
 
 for YML in $(find $PWD -d -name "*.yml")
 do
@@ -13,3 +25,5 @@ do
       java -jar $jar -XX:+UseSerialGC -Xmx1G Antigen
     echo "*** Completed $YML"
 done
+
+echo "LOG: Simulations complete!";
