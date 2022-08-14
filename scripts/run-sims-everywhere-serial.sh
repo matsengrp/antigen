bf35b995d9b1884a5668e1ad02e41e149f3c14b0
zorian15 <zoriant15@gmail.com>
2022-08-14 09:23:11 -0700

    file rename

--- scripts/run-sims-everywhere-serial.sh
+++ scripts/run-sims-everywhere-serial.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -eu
-SCRIPT_PATH="/path/to/script.sh"
+
+jar=$(find $PWD -name "antigen.jar");
 
 # Move to desired ouput directory
 cd ../python/results
@@ -9,6 +10,6 @@ for YML in $(find $PWD -d -name "*.yml")
 do
     cd $(dirname $YML)
     echo "\n*** Starting $YML"
-      java -jar ../../../antigen.jar -XX:+UseSerialGC -Xmx1G Antigen
+      java -jar $jar -XX:+UseSerialGC -Xmx1G Antigen
     echo "*** Completed $YML"
 done
